73017bfacbc879cdc4b4097474dbd4e465f2c69d
alXmas <alexsandr.xmas@gmail.com>
2020-03-26 13:50:33 +0400

    solved the problems

--- white_belt/week-02/task-07/main.cpp
+++ white_belt/week-02/task-07/main.cpp
@@ -3,13 +3,13 @@
 
 using namespace std;
 
-vector<int> Reversed(const vector<int>& input)
-{
-    vector<int> result;
-    for (int i = input.size() - 1; i >= 0; --i) {
-        result.push_back(input[i]);
-    }
-    return result;
+void Reverse(vector<int>& v) {
+  int n = v.size();
+  for (int i = 0; i < n / 2; ++i) {
+    int tmp = v[i];
+    v[i] = v[n - 1 - i];
+    v[n - 1 - i] = tmp;
+  }
 }
 
 int main(){
