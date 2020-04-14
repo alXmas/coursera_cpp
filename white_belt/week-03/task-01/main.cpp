66572b41b77278291758b0e1c07f674bc2c1425f
alXmas <alexsandr.xmas@gmail.com>
2020-04-14 10:27:14 +0400

    solved the problem

--- white_belt/week-03/task-01/main.cpp
+++ white_belt/week-03/task-01/main.cpp
@@ -1,8 +1,25 @@
 #include <iostream>
+#include <vector>
+#include <algorithm> 
 
 using namespace std;
 
-int main(){
+bool myfunction (int i,int j) { return (abs(i)  < abs(j)); }
 
+int main(){
+  int n;
+  cin >> n;
+
+  vector<int> v(n);
+
+  for (int& s : v) {
+      cin >> s;
+  }
+
+  sort(v.begin(), v.end(), myfunction);
+    
+  for(const auto& i:v){
+      cout << i << " ";
+  }
   return 0;
 }
