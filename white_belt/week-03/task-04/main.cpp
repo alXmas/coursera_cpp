a58b077b11e1a6369f1baec0a930f9e8935c67f4
alXmas <alexsandr.xmas@gmail.com>
2020-04-17 08:28:37 +0400

    solved the problem

--- white_belt/week-03/task-04/main.cpp
+++ white_belt/week-03/task-04/main.cpp
@@ -1,33 +1,70 @@
 #include <iostream>
-#include <vector>
 #include <string>
-#include <algorithm> 
+#include <map>
+#include <algorithm>
 
 using namespace std;
 
-class SortedStrings {
+class Person {
 public:
-  void AddString(const string& s) {
-    data.push_back(s);
+  void ChangeFirstName(int year, const string& first_name) {
+    firstname[year] = first_name;
   }
-  vector<string> GetSortedStrings() {
-    sort(begin(data), end(data));
-    return data;
+  void ChangeLastName(int year, const string& last_name) {
+    lastname[year] = last_name;
+  }
+  string GetFullName(int year) {
+      string first_name = GetFirstName(year, firstname);
+      string last_name = GetLastName(year, lastname);
+      string full_name = first_name + " " + last_name;
+      if (first_name.size() == 0 && last_name.size() == 0) {
+          full_name = "Incognito";
+          return full_name;
+      } else if (first_name.size() == 0) {
+          full_name = last_name + " with unknown first name";
+          return full_name;
+      } else if (last_name.size() == 0) {
+          full_name = first_name + " with unknown last name";
+          return full_name;
+      }
+      return full_name;
   }
 private:
-  vector<string> data;
+    string GetFirstName(int year, map<int, string>& firstname) {
+        while (firstname.count(year) == 0 && year > 0) {
+            --year;
+        }
+        return firstname[year];
+    }
+    string GetLastName(int year, map<int, string>& lastname) {
+        while (lastname.count(year) == 0 && year > 0) {
+            --year;
+        }
+        return lastname[year];
+    }
+
+    map<int, string> firstname;
+    map<int, string> lastname;
 };
 
 int main(){
-  SortedStrings strings;
+  Person person;
   
-  strings.AddString("first");
-  strings.AddString("third");
-  strings.AddString("second");
-  strings.GetSortedStrings();
+  person.ChangeFirstName(1965, "Polina");
+  person.ChangeLastName(1967, "Sergeeva");
+  for (int year : {1900, 1965, 1990}) {
+    cout << person.GetFullName(year) << endl;
+  }
   
-  strings.AddString("second");
-  strings.GetSortedStrings();
-
+  person.ChangeFirstName(1970, "Appolinaria");
+  for (int year : {1969, 1970}) {
+    cout << person.GetFullName(year) << endl;
+  }
+  
+  person.ChangeLastName(1968, "Volkova");
+  for (int year : {1969, 1970}) {
+    cout << person.GetFullName(year) << endl;
+  }
+  
   return 0;
 }
